eed4165ed7efddd5653677b3d73a42080270e248
Joshaline <joshuachinwendu@gmail.com>
2021-09-24 15:54:51 +0100

    added google-auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv') //virtual environment
+const exphbs  = require('express-handlebars');
 const connectDB = require('./database/connection');
 
 // Initialize express
@@ -12,18 +13,22 @@ dotenv.config({ path: './config/config.env' })
 // Initialize DB
 connectDB()
 
+// Template engine (handlebars)
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/courseEntries', require('./routes/courseEntries'))
 
 
 
 
-// Body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-// Get Home page
-app.get('/', (req, res) => {
-  res.send('hellow')
-});
 
 
 
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+
+// @desc    Login/Landing page
+// @route   GET /
+router.get('/', (req, res) => {
+  res.render('login', {
+    layout: 'login',
+  })
+})
+
+
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard', {
+    layout: 'mains',
+  })
+})
+
+
+
+module.exports = router
